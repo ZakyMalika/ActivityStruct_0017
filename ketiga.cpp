e1f2cf4f0f1e3d7ed8fbc794c58d5f2a58ea2f7e
ZakyMalika <zakymalika0987@gmail.com>
2024-04-02 17:29:29 +0700

    di int main menggunakan getline karena di nama mahasiswa kita ingin menuliskannya 2 kali

--- ketiga.cpp
+++ ketiga.cpp
@@ -12,3 +12,11 @@ struct mahasiswa {
 	DetailAlamat alamat;
 	int umur;
 };
+
+int main() {
+	mahasiswa mhs;
+	cout << "Nomor Mahasiswa : ";
+	getline(cin, mhs.nim); //error di vsc tidak error
+	cout << "Nama Mahasiswa : ";
+	getline(cin, mhs.nama);
+}
