b5ec86aeb1467b76534bbee9aaf21776ea87363e
ZakyMalika <zakymalika0987@gmail.com>
2024-04-02 17:51:29 +0700

    memeperbaiki yang salah

--- keempat.cpp
+++ keempat.cpp
@@ -16,34 +16,36 @@ struct mahasiswa {
 
 int main() {
 	mahasiswa mhs[3];
-    for(int i=0;i<3;i++){
-	cout << "Nomor Mahasiswa : ";
-	getline(cin, mhs[i].nim); //error di vsc tidak error
-	cout << "Nama Mahasiswa : ";
-	getline(cin, mhs[i].nama);
+    for(int i=0;i<3;i++)
+    {
+        cout << "Data Ke- " <<(i + 1) <<":" << endl;
+        cout << "Nomor Mahasiswa : ";
+        getline(cin, mhs[i].nim); //error di vsc tidak error
+        cout << "Nama Mahasiswa : ";
+        getline(cin, mhs[i].nama);
 
-	cout << "Alamat Mahasiswa : " << endl;
-	cout << "\t Nama Desa : ";
-	cin >> mhs[i].alamat.desa;
-	cout << "\t Nama Kota : ";
-	cin >> mhs[i].alamat.kota;
+        cout << "Alamat Mahasiswa : " << endl;
+        cout << "\t Nama Desa : ";
+        cin >> mhs[i].alamat.desa;
+        cout << "\t Nama Kota : ";
+        cin >> mhs[i].alamat.kota;
 
-	cout << "Umur Mahasiswa : ";
-	cin >> mhs[i].umur;
-    cin.ignore(1, '\n');
-    cout << endl;
+        cout << "Umur Mahasiswa : ";
+        cin >> mhs[i].umur;
+        cin.ignore(1, '\n');
+        cout << endl;
     }
 
-    for(int i=0;i<3;i++){
-    
-	cout << endl;
-    cout << "Data Mahasiswa ke- " <<(i+1) <<endl;
-	cout << "\n NIM : " << mhs[i].nim;
-	cout << "\n Nama : " << mhs[i].nama;
-	cout << "\n Alamat : ";
-	cout << "\n \t Nama Desa : " << mhs[i].alamat.desa;
-	cout << "\n \t Nama Kota : " << mhs[i].alamat.kota;
-	cout << "\n Umur : " << mhs[i].umur;
-    cout << endl;
+    for(int i=0;i<3;i++)
+    {
+        cout << endl;
+        cout << "Data Mahasiswa ke- " <<(i+1) <<endl;
+        cout << "\n NIM : " << mhs[i].nim;
+        cout << "\n Nama : " << mhs[i].nama;
+        cout << "\n Alamat : ";
+        cout << "\n \t Nama Desa : " << mhs[i].alamat.desa;
+        cout << "\n \t Nama Kota : " << mhs[i].alamat.kota;
+        cout << "\n Umur : " << mhs[i].umur;
+        cout << endl;
     }
 }
